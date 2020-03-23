ed85a169d5b475911b517f43795b03cfb6a0c869
Shiva Giri <47266376+Shiva-Giri@users.noreply.github.com>
2020-03-23 22:03:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/map/NewService.java
@@ -0,0 +1,48 @@
+package com.example.map;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class NewService extends Service implements LocationListener {
+
+    Context context;
+
+    public NewService(Context context) {
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        ( (MapsActivity) context).setMarker(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
